455da85b72d69dd90bf8a7a05a78343d2a8645da
bangjaeyoung <qkdwo4613@gmail.com>
2024-01-03 15:47:14 +0900

    refactor: news 도메인 전체 단순 수정

---
+++ server/src/main/java/mainproject/stocksite/domain/news/dto/NewsDto.java
@@ -0,0 +1,35 @@
+package mainproject.stocksite.domain.news.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class NewsDto {
+    
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private String lastBuildDate;
+        private Integer total;
+        private Integer start;
+        private Integer display;
+        private List<Item> items = null;
+        
+        @Getter
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        private static class Item {
+            private String title;
+            private String originallink;
+            private String link;
+            private String description;
+            private String pubDate;
+        }
+    }
+}
--- server/src/main/java/mainproject/stocksite/domain/news/service/NewsService.java
+++ server/src/main/java/mainproject/stocksite/domain/news/service/NewsService.java
@@ -1,8 +1,7 @@
 package mainproject.stocksite.domain.news.service;
 
 import lombok.RequiredArgsConstructor;
-import mainproject.stocksite.domain.news.dto.NewsResponseDto;
-import mainproject.stocksite.domain.news.options.NewsOptions;
+import mainproject.stocksite.domain.news.dto.NewsDto;
 import mainproject.stocksite.global.config.OpenApiSecretInfo;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,44 +12,51 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@RequiredArgsConstructor
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class NewsService {
-
+    private static final String NAVER_DEFAULT_URL = "https://openapi.naver.com/v1/search/";
+    
     private final OpenApiSecretInfo openApiSecretInfo;
-
-    private final String NAVER_DEFAULT_URL = "https://openapi.naver.com/v1/search/";
-
     private final RestTemplate restTemplate;
-
+    
     private HttpHeaders baseHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Naver-Client-Id", openApiSecretInfo.getNaverClientId());
         headers.set("X-Naver-Client-Secret", openApiSecretInfo.getNaverClientSecret());
+        
         return headers;
     }
-
-    public NewsResponseDto searchStockNews(NewsOptions newsOptions) {
+    
+    public NewsDto.Response searchStockNews(
+            String search,
+            int count,
+            int start,
+            String sort) {
+        
         HttpHeaders requestHeaders = baseHeaders();
         requestHeaders.set("Content-Type", "application/json");
+        
         HttpEntity<String> requestMessage = new HttpEntity<>(requestHeaders);
-
         String url = NAVER_DEFAULT_URL + "news";
-
+        
         UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
-                .queryParam("query", newsOptions.getSearch())
-                .queryParam("display", newsOptions.getCount())
-                .queryParam("start", newsOptions.getStart())
-                .queryParam("sort", newsOptions.getSort())
+                .queryParam("query", search)
+                .queryParam("display", count)
+                .queryParam("start", start)
+                .queryParam("sort", sort)
                 .build();
-
-        ResponseEntity<NewsResponseDto> response = restTemplate.exchange(
+        
+        ResponseEntity<NewsDto.Response> response = restTemplate.exchange(
                 uriBuilder.toString(),
                 HttpMethod.GET,
                 requestMessage,
-                NewsResponseDto.class
+                NewsDto.Response.class
         );
-
+        
         return response.getBody();
     }
 }
