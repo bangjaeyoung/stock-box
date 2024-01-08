8bd9e61b6310fdf1037bf194df1dd0327cc0f353
bangjaeyoung <qkdwo4613@gmail.com>
2024-01-08 15:57:20 +0900

    refactor: redis cache 기본 설정

--- server/src/main/java/mainproject/stocksite/StocksiteApplication.java
+++ server/src/main/java/mainproject/stocksite/StocksiteApplication.java
@@ -4,16 +4,18 @@ import mainproject.stocksite.global.config.OpenApiSecretInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
-@EnableConfigurationProperties(OpenApiSecretInfo.class)
 @SpringBootApplication
+@EnableConfigurationProperties(OpenApiSecretInfo.class)
 public class StocksiteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StocksiteApplication.class, args);
-	}
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StocksiteApplication.class, args);
+    }
 }
