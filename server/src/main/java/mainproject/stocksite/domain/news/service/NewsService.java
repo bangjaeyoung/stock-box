feba1bc1ffcbe1cc6097e0c77b9469f29ed81165
bangjaeyoung <qkdwo4613@gmail.com>
2024-01-11 02:01:30 +0900

    refactor: news 조회 로직 예외 처리 추가

--- server/src/main/java/mainproject/stocksite/domain/news/service/NewsService.java
+++ server/src/main/java/mainproject/stocksite/domain/news/service/NewsService.java
@@ -3,33 +3,30 @@ package mainproject.stocksite.domain.news.service;
 import lombok.RequiredArgsConstructor;
 import mainproject.stocksite.domain.news.dto.NewsDto;
 import mainproject.stocksite.global.config.OpenApiSecretInfo;
+import mainproject.stocksite.global.exception.BusinessLogicException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
 
+import static mainproject.stocksite.global.exception.ExceptionCode.OPEN_API_SERVER_ERROR;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class NewsService {
-    private static final String NAVER_DEFAULT_URL = "https://openapi.naver.com/v1/search/";
+    private static final String NAVER_DEFAULT_URL = "https://openapi.naver.com/v1/search/news";
     
     private final OpenApiSecretInfo openApiSecretInfo;
     private final RestTemplate restTemplate;
-    
-    private HttpHeaders baseHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Naver-Client-Id", openApiSecretInfo.getNaverClientId());
-        headers.set("X-Naver-Client-Secret", openApiSecretInfo.getNaverClientSecret());
-        
-        return headers;
-    }
     
     public NewsDto.Response searchStockNews(
             String search,
@@ -37,26 +34,35 @@ public class NewsService {
             int start,
             String sort) {
         
-        HttpHeaders requestHeaders = baseHeaders();
-        requestHeaders.set("Content-Type", "application/json");
+        HttpEntity<String> requestMessage = new HttpEntity<>(getBaseHeaders());
         
-        HttpEntity<String> requestMessage = new HttpEntity<>(requestHeaders);
-        String url = NAVER_DEFAULT_URL + "news";
-        
-        UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
+        UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(NAVER_DEFAULT_URL)
                 .queryParam("query", search)
                 .queryParam("display", count)
                 .queryParam("start", start)
                 .queryParam("sort", sort)
                 .build();
         
-        ResponseEntity<NewsDto.Response> response = restTemplate.exchange(
-                uriBuilder.toString(),
-                HttpMethod.GET,
-                requestMessage,
-                NewsDto.Response.class
-        );
+        ResponseEntity<NewsDto.Response> response;
+        try {
+            response = restTemplate.exchange(
+                    uriBuilder.toString(),
+                    HttpMethod.GET,
+                    requestMessage,
+                    NewsDto.Response.class
+            );
+        } catch (HttpClientErrorException | HttpServerErrorException openApiServerError) {
+            throw new BusinessLogicException(OPEN_API_SERVER_ERROR);
+        }
         
         return response.getBody();
+    }
+    
+    private HttpHeaders getBaseHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", openApiSecretInfo.getNaverClientId());
+        headers.set("X-Naver-Client-Secret", openApiSecretInfo.getNaverClientSecret());
+        headers.set("Content-Type", "application/json");
+        return headers;
     }
 }
