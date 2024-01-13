2757834da42b1a506d5824d48385f626c67688bf
bangjaeyoung <qkdwo4613@gmail.com>
2024-01-13 18:57:07 +0900

    refactor: DateConfig 클래스 로직 개선

--- server/src/main/java/mainproject/stocksite/domain/stock/overall/config/DateConfig.java
+++ server/src/main/java/mainproject/stocksite/domain/stock/overall/config/DateConfig.java
@@ -3,22 +3,23 @@ package mainproject.stocksite.domain.stock.overall.config;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+/**
+ * PackageName: mainproject.stocksite.domain.stock.overall.config
+ * FileName: DateConfig
+ * Author: bangjaeyoung
+ * Date: 2024-01-12
+ * Description: Open API 서버로부터 현재부터 5일전까지의 데이터를 가져오기 위해, 현재부터 5일전까지의 날짜를 구하기 위한 클래스
+ */
 @Getter
 @Component
 public class DateConfig {
-
-    public String getFromFiveDaysAgoToNow() {
-        Calendar cal = Calendar.getInstance();
-
-        SimpleDateFormat sdformat = new SimpleDateFormat("yyyyMMdd");
-
-        cal.add(Calendar.DATE, -5);
-
-        String fromFiveDaysAgoToNow = sdformat.format(cal.getTime());
-
-        return fromFiveDaysAgoToNow;
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+    
+    public static String getFiveDaysAgoToNow() {
+        LocalDate fiveDaysAgo = LocalDate.now().minusDays(5);
+        return DATE_FORMATTER.format(fiveDaysAgo);
     }
 }
